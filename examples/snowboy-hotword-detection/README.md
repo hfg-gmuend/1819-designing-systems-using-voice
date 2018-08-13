38bcc71357e456e39a3e8fcb4315b9162bcd27ba
Benedikt Groß <bg@benedikt-gross.de>
2018-08-13 14:07:51 +0200

    Added snowboy-hotword-detection example

---
+++ examples/snowboy-hotword-detection/README.md
@@ -0,0 +1,18 @@
+# Snowboy Hotword Detector
+Hotword detector example for [Node.js](https://nodejs.org/) using [Snowboy](https://snowboy.kitt.ai/). Snowboy is an offline neural network driven hotword detection library, with compatibility on macOS and most Linux's.
+
+Example is based on the [node-hotworddetector](https://github.com/RedKenrok/node-hotworddetector) a simplified wrapper around the official [snowboy module](https://github.com/Kitt-AI/snowboy).
+
+## Installation
+1. Install the prerequisites  below
+2. `npm install --save`
+
+## Prerequisites
+This module requires you to install [SoX](http://sox.sourceforge.net/)
+
+### For MacOS
+```
+brew install sox --with-flac
+```
+
+You can test whether SoX was installed correctly with e.g. `rec --channels=1 --bits=16 --rate=16000 audio.flac trim 0 5`.
---
+++ examples/snowboy-hotword-detection/example.js
@@ -0,0 +1,34 @@
+const HotwordDetector = require('node-hotworddetector');
+
+const detectorData = {
+  resource: './node_modules/snowboy/resources/common.res'
+};
+const modelData = [{
+  file: './node_modules/snowboy/resources/snowboy.umdl',
+  hotwords : 'snowboy'
+}];
+
+let hotwordDetector = new HotwordDetector(detectorData, modelData, {audioGain: 2}, console);
+
+hotwordDetector.on('error', function(error) {
+  console.error('Error: ' + error);
+});
+
+hotwordDetector.on('hotword', function(index, hotword, buffer) {
+  console.log('Hotword: ' + hotword);
+});
+
+hotwordDetector.on('silence', function() {
+  console.log('Silence');
+});
+
+hotwordDetector.on('sound', function(buffer) {
+  console.log('Sound');
+});
+
+hotwordDetector.start();
+
+console.log('Active hotwords 🎤 :');
+for (model of modelData) {
+  console.log(model.hotwords);
+}
